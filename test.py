19fed37110959926b9ad27428f6f69632e8bc477
ooooocean <hophuong.ocean@gmail.com>
2024-03-08 15:19:08 +0000

    added unit test for popping empty stack

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import main
+import pytest
+
 
 def test_create_stack_accepts_single_word():
-    assert main.create_text_stack('text') == ['t', 'e', 'x', 't']#
+    assert main.create_text_stack('text') == ['t', 'e', 'x', 't']  #
 
-def test_empty_stack():
+
+class TestStackIsEmpty:
     x = []
-    assert main.stack_is_empty(x) is True
 
-def test_non_empty_stack():
-    x = ['t', 'e', 's', 't']
-    assert main.stack_is_empty(x) is False
+    def test_empty_stack(self):
+        assert main.stack_is_empty(self.x) is True
+
+
+    def test_pop_empty_stack(self):
+        with pytest.raises(IndexError):
+            main.pop_stack(self.x, 'z')
+
+
+class TestStackIsNotEmpty:
+    def test_non_empty_stack(self):
+        x = ['t', 'e', 's', 't']
+        assert main.stack_is_empty(x) is False
