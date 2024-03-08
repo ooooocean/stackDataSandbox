56f6e5fd2e95bc6be0d8d4d020a3edf6b8007767
ooooocean <hophuong.ocean@gmail.com>
2024-03-08 18:10:27 +0000

    saving changes to reverse individual words function

--- main.py
+++ main.py
@@ -43,3 +43,36 @@ def reverse_stack_with_recursion(stack, string):
     else:
         string += pop_stack(stack)
         reverse_stack_with_recursion(stack, string)
+
+
+def reverse_individual_words_in_stack(words):
+    words_stack = create_stack(words)
+    new_words = []
+    buffer_stack = []
+    # iterate until the stack is empty
+    while words_stack:
+        # add letters to the new stack from our input
+        print(f'Original Stack is {words_stack}')
+        new_words.append(pop_stack(words_stack))
+        print(f'New stack is {new_words}\n'
+              f'Original stack is now {words_stack}.\n')
+        # when a space is found, pop the stack to a buffer for later
+        if peek_stack(words_stack) == ' ':
+            # iterate through our new stack until it is empty
+            while new_words:
+                # pop the space from the new stack to retain word order
+                pop_stack(new_words)
+                # move words to buffer
+                buffer_stack += pop_stack(new_words)
+            # once input is empty, add a space to the top of the buffer stack to preserve spacing
+            else:
+                push_stack(buffer_stack, ' ')
+    # once words have all been iterated through, add words back in from the buffer
+    else:
+        while buffer_stack:
+            new_words.append(pop_stack(buffer_stack))
+        # once buffer_stack is empty, print the new stack
+        else:
+            return new_words
+
+print(reverse_individual_words_in_stack('Hello World'))
