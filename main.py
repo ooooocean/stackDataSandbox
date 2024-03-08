7622dbe0373026b446aa238b54e8a9c64abb6b85
ooooocean <hophuong.ocean@gmail.com>
2024-03-08 14:48:30 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Playing around with stack data structure, using it to reverse letters.
+"""
+
+
+def create_text_stack(stack):
+    stack=[x for x in stack]
+    return stack
+
+def stack_empty(stack):
+    if stack == []:
+        return True
+    else:
+        return False
+
+test = 'text'
+print(create_text_stack(test))
