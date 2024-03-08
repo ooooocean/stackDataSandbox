75ea3aa10a6667174c990ed5ab989a36a45a7c76
ooooocean <hophuong.ocean@gmail.com>
2024-03-08 15:09:37 +0000

    removed class from unit tests

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ import main
 def test_create_stack_accepts_single_word():
     assert main.create_text_stack('text') == ['t', 'e', 'x', 't']#
 
-class TestIsEmpty:
-    def test_empty_stack(self):
-        x = []
-        assert main.stack_is_empty(x) is True
+def test_empty_stack():
+    x = []
+    assert main.stack_is_empty(x) is True
 
-    def test_non_empty_stack(self):
-        x = ['t', 'e', 's', 't']
-        assert main.stack_is_empty(x) is False
+def test_non_empty_stack():
+    x = ['t', 'e', 's', 't']
+    assert main.stack_is_empty(x) is False
